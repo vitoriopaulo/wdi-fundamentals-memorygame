90e4fca0514bfc75801dfa34947c0d250ec5d56d
Vitorio Paulo <vitoriopaulo@gmail.com>
2017-08-04 11:15:33 -0400

    createdgame logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,11 +1,21 @@
-var cardOne = "queen";
-
-var cardTwo = "queen";
-
-var cardThree = "king";
-
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardThree);
+while (cardsInPlay < 2)
+	{cardsInPlay += 1}
+	console.log(cardsInPlay);
+
+
+if (cardOne === cardTwo) {alert("You found a match");}
+else {alert("Sorry, try again");}
+
+
+
+
