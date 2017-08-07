86eb7e3fcad70de5efbd061a3aaa723c93f811c6
Vitorio Paulo <vitoriopaulo@gmail.com>
2017-08-07 15:26:34 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,30 @@
 console.log("Up and running!");
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+{
+rank: "Queen",
+suite: "Hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+
+{
+rank: "Queen",
+suite: "Diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "King",
+suite: "Hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "King",
+suite: "Diamonds",
+cardImage: "images/king-of-diamonds.png"	
+}
+];
 
 var cardsInPlay = [];
 
@@ -15,8 +39,10 @@ var checkForMatch = function() {
 
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suite);
 
 
 if (cardsInPlay.length === 2) {
