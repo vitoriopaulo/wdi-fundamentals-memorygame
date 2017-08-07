4e9064fa7898bad34acec63095035724c8ef435d
Vitorio Paulo <vitoriopaulo@gmail.com>
2017-08-07 14:56:41 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -4,22 +4,25 @@ var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match! Congrats! :)");
 	} else {
 		alert("Sorry, try again. :(");
-	};
-};
+	}
+}
+
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
